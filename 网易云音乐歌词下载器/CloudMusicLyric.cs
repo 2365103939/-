bfa2018d9fcef4254265b1ec5cff51ec36724fc8
wwh1004 <wwh1004@hotmail.com>
2018-03-15 23:36:50 +0800

    v1.0.0.0

---
+++ 网易云音乐歌词下载器/CloudMusicLyric.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace 网易云音乐歌词下载器
+{
+    internal static class CloudMusicLyric
+    {
+        private static readonly byte[] _163Start = Encoding.UTF8.GetBytes("163 key(Don't modify):");
+        private static readonly byte[] _163EndMp3 = { 0x54, 0x41, 0x4C, 0x42 };
+        private static readonly byte[] _163EndFlac = { 0x0, 0x0, 0x0, 0x45 };
+        private static readonly byte[] Key = Encoding.UTF8.GetBytes(@"#14ljk_!\]&0U<'(");
+        private static readonly AesCryptoServiceProvider AesDecryptor = new AesCryptoServiceProvider
+        {
+            BlockSize = 128,
+            Key = Key,
+            Mode = CipherMode.ECB,
+            Padding = PaddingMode.PKCS7
+        };
+        private static readonly byte[] UnCollectedFlag = Encoding.UTF8.GetBytes("\"uncollected\":true");
+        private static readonly byte[] NoLyricFlag = Encoding.UTF8.GetBytes("\"nolyric\":true");
+        private static readonly byte[] Lrc = Encoding.UTF8.GetBytes("\"lrc\":{\"");
+        private static readonly byte[] TLyric = Encoding.UTF8.GetBytes("\"tlyric\":{\"");
+        private static readonly byte[] Lyric = Encoding.UTF8.GetBytes("\"lyric\":");
+        private static readonly byte[] LyricEnd = Encoding.UTF8.GetBytes("},");
+
+        public static byte[] Get163Key(string file, bool isMp3)
+        {
+            byte[] bytFile;
+            int startIndex;
+            int endIndex;
+            byte[] byt163Key;
+
+            bytFile = new byte[0x4000];
+            using (FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read))
+                fileStream.Read(bytFile, 0, bytFile.Length);
+            startIndex = GetIndex(bytFile, _163Start, 0);
+            if (startIndex == -1)
+                return null;
+            if (isMp3)
+                endIndex = GetIndex(bytFile, _163EndMp3, startIndex);
+            else
+                endIndex = GetIndex(bytFile, _163EndFlac, startIndex) - 1;
+            if (endIndex == -1)
+                return null;
+            byt163Key = new byte[endIndex - startIndex - _163Start.Length];
+            Buffer.BlockCopy(bytFile, startIndex + _163Start.Length, byt163Key, 0, byt163Key.Length);
+            return byt163Key;
+        }
+
+        public static string GetMusicId(byte[] byt163Key)
+        {
+            byt163Key = Convert.FromBase64String(Encoding.UTF8.GetString(byt163Key));
+            byt163Key = AesDecryptor.CreateDecryptor().TransformFinalBlock(byt163Key, 0, byt163Key.Length);
+            return Encoding.UTF8.GetString(byt163Key, 17, GetIndex(byt163Key, 0x2C, 17) - 17);
+        }
+
+        public static byte[] GetLyric(string musicId)
+        {
+            return GetWebSrc("http://music.163.com/api/song/lyric?os=pc&id=" + musicId + "&lv=-1&tv=-1");
+        }
+
+        public static bool IsCollected(byte[] bytLyric)
+        {
+            return GetIndex(bytLyric, UnCollectedFlag, 1, 1) != 1;
+        }
+
+        public static bool HasLyric(byte[] bytLyric)
+        {
+            return GetIndex(bytLyric, NoLyricFlag, 1, 1) != 1;
+        }
+
+        public static string GetOriginalLyric(byte[] bytLyric)
+        {
+            int startIndex;
+            int endIndex;
+            int subStartIndex;
+            string strLrc;
+
+            startIndex = GetIndex(bytLyric, Lrc, 0);
+            endIndex = GetIndex(bytLyric, LyricEnd, startIndex);
+            subStartIndex = GetIndex(bytLyric, Lyric, startIndex, endIndex) + Lyric.Length;
+            strLrc = Encoding.UTF8.GetString(bytLyric, subStartIndex, endIndex - subStartIndex);
+            return strLrc == "null" ? null : Regex.Unescape(strLrc.Substring(1, strLrc.Length - 2));
+        }
+
+        public static string GetTranslatedLyric(byte[] bytLyric)
+        {
+            int startIndex;
+            int endIndex;
+            int subStartIndex;
+            string strLrc;
+
+            startIndex = GetIndex(bytLyric, TLyric, 0);
+            endIndex = GetIndex(bytLyric, LyricEnd, startIndex);
+            subStartIndex = GetIndex(bytLyric, Lyric, startIndex, endIndex) + Lyric.Length;
+            strLrc = Encoding.UTF8.GetString(bytLyric, subStartIndex, endIndex - subStartIndex);
+            return strLrc == "null" ? null : Regex.Unescape(strLrc.Substring(1, strLrc.Length - 2));
+        }
+
+        private static int GetIndex(byte[] src, byte dest, int startIndex)
+        {
+            return GetIndex(src, dest, startIndex, src.Length - 1);
+        }
+
+        private static int GetIndex(byte[] src, byte dest, int startIndex, int endIndex)
+        {
+            for (int i = startIndex; i < endIndex + 1; i++)
+                if (src[i] == dest)
+                    return i;
+            return -1;
+        }
+
+        private static int GetIndex(byte[] src, byte[] dest, int startIndex)
+        {
+            return GetIndex(src, dest, startIndex, src.Length - dest.Length);
+        }
+
+        private static int GetIndex(byte[] src, byte[] dest, int startIndex, int endIndex)
+        {
+            int j;
+
+            for (int i = startIndex; i < endIndex + 1; i++)
+                if (src[i] == dest[0])
+                {
+                    for (j = 1; j < dest.Length; j++)
+                        if (src[i + j] != dest[j])
+                            break;
+                    if (j == dest.Length)
+                        return i;
+                }
+            return -1;
+        }
+
+        private static byte[] GetWebSrc(string url)
+        {
+            Stream stream;
+            byte[] buffer;
+            List<byte> byteList;
+            int count;
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)((HttpWebRequest)WebRequest.Create(url)).GetResponse())
+                {
+                    stream = response.GetResponseStream();
+                    buffer = new byte[0x1000];
+                    byteList = new List<byte>();
+                    for (int i = 0; i < int.MaxValue; i++)
+                    {
+                        count = stream.Read(buffer, 0, buffer.Length);
+                        if (count == 0x1000)
+                            byteList.AddRange(buffer);
+                        else if (count == 0)
+                            return byteList.ToArray();
+                        else
+                            for (int j = 0; j < count; j++)
+                                byteList.Add(buffer[j]);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            throw new OutOfMemoryException();
+        }
+    }
+}
--- 网易云音乐歌词下载器/Program.cs
+++ 网易云音乐歌词下载器/Program.cs
@@ -1,23 +1,187 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ServiceProcess;
+using System.IO;
 using System.Text;
 
 namespace 网易云音乐歌词下载器
 {
-    static class Program
+    internal static class Program
     {
+        private static Encoding _Encoding;
+
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
-        static void Main()
+        private static void Main()
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
+            string arg;
+            string floder;
+            int lyricMode;
+            string extension;
+            bool isMp3;
+            string fileName;
+            string lrcFile;
+            byte[] byt163Key;
+            string musicId;
+            byte[] bytLyric;
+            bool hasLyric;
+            string lrc;
+            string tlrc;
+
+            Console.Title = "网易云音乐歌词下载器";
+#if DEBUG
+            floder = @"G:\Music";
+            _Encoding = Encoding.UTF8;
+            lyricMode = 1;
+#else
+            Console.WriteLine("输入要同步歌词的文件夹：");
+            inputFloder:
+            floder = Console.ReadLine();
+            if (!Directory.Exists(floder))
             {
-                new Service1()
-            };
-            ServiceBase.Run(ServicesToRun);
+                Console.WriteLine("文件夹不存在，请重新输入：");
+                goto inputFloder;
+            }
+            Console.WriteLine("使用指定编码保存歌词文件（留空使用默认编码UTF-8）：");
+            inputEncoding:
+            arg = Console.ReadLine();
+            if (arg == string.Empty)
+                _Encoding = Encoding.UTF8;
+            else
+                try
+                {
+                    _Encoding = Encoding.GetEncoding(arg);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("编码不存在，请重新输入：");
+                    goto inputEncoding;
+                }
+            Console.WriteLine("是否使用原始歌词（输入“是”优先使用原始歌词，输入“否”优先使用翻译歌词，留空将逐一选择）：");
+            inputLyricMode:
+            arg = Console.ReadLine();
+            if (arg == string.Empty)
+                lyricMode = 0;
+            else if (arg == "是")
+                lyricMode = 1;
+            else if (arg == "否")
+                lyricMode = 2;
+            else
+            {
+                Console.WriteLine("输入有误，请重新输入：");
+                goto inputLyricMode;
+            }
+#endif
+            foreach (string file in Directory.GetFiles(floder))
+            {
+                extension = Path.GetExtension(file).ToUpperInvariant();
+                if (extension == ".MP3")
+                    isMp3 = true;
+                else if (extension == ".FLAC")
+                    isMp3 = false;
+                else
+                    continue;
+                fileName = Path.GetFileName(file);
+                lrcFile = Path.ChangeExtension(file, ".lrc");
+                if (File.Exists(lrcFile))
+                {
+                    Console.WriteLine(fileName + "的歌词文件已存在");
+                    continue;
+                }
+                byt163Key = CloudMusicLyric.Get163Key(file, isMp3);
+                if (byt163Key == null)
+                {
+                    Console.WriteLine("无法获取文件" + fileName + "的163Key，请手动输入歌曲ID下载歌词（留空将跳过此歌曲）：");
+                    inputMusicId:
+                    musicId = Console.ReadLine();
+                    if (musicId == string.Empty)
+                        continue;
+                    else
+                    {
+                        if (int.TryParse(musicId, out int num))
+                            goto getBytLyric;
+                        else
+                        {
+                            Console.WriteLine("输入有误，请重新输入：");
+                            goto inputMusicId;
+                        }
+                    }
+                }
+                musicId = CloudMusicLyric.GetMusicId(byt163Key);
+                getBytLyric:
+                bytLyric = CloudMusicLyric.GetLyric(musicId);
+                if (!CloudMusicLyric.IsCollected(bytLyric))
+                {
+                    Console.WriteLine(fileName + "的歌曲ID未被收录，请手动输入歌曲ID下载歌词（留空将跳过此歌曲）：");
+                    inputMusicId:
+                    musicId = Console.ReadLine();
+                    if (musicId == string.Empty)
+                        continue;
+                    else
+                    {
+                        if (int.TryParse(musicId, out int num))
+                            goto getBytLyric;
+                        else
+                        {
+                            Console.WriteLine("输入有误，请重新输入：");
+                            goto inputMusicId;
+                        }
+                    }
+                }
+                hasLyric = CloudMusicLyric.HasLyric(bytLyric);
+                if (hasLyric)
+                {
+                    lrc = CloudMusicLyric.GetOriginalLyric(bytLyric);
+                    tlrc = CloudMusicLyric.GetTranslatedLyric(bytLyric);
+                    if (lrc == null)
+                    {
+                        File.WriteAllText(lrcFile, tlrc, _Encoding);
+                        Console.WriteLine(Path.ChangeExtension(fileName, ".lrc") + "下载成功，使用翻译歌词");
+                    }
+                    else if (tlrc == null)
+                    {
+                        File.WriteAllText(lrcFile, lrc, _Encoding);
+                        Console.WriteLine(Path.ChangeExtension(fileName, ".lrc") + "下载成功，使用原始歌词");
+                    }
+                    else
+                    {
+                        switch (lyricMode)
+                        {
+                            case 0:
+                                Console.WriteLine(fileName + "是否使用原始歌词（输入“是”使用原始歌词，输入“否”使用翻译歌词）：");
+                                inputIfUsingOriginal:
+                                arg = Console.ReadLine();
+                                if (arg == "是")
+                                {
+                                    File.WriteAllText(lrcFile, lrc, _Encoding);
+                                    Console.WriteLine(Path.ChangeExtension(fileName, ".lrc") + "下载成功，使用原始歌词");
+                                }
+                                else if (arg == "否")
+                                {
+                                    File.WriteAllText(lrcFile, tlrc, _Encoding);
+                                    Console.WriteLine(Path.ChangeExtension(fileName, ".lrc") + "下载成功，使用翻译歌词");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("输入有误，请重新输入：");
+                                    goto inputIfUsingOriginal;
+                                }
+                                break;
+                            case 1:
+                                File.WriteAllText(lrcFile, lrc, _Encoding);
+                                Console.WriteLine(Path.ChangeExtension(fileName, ".lrc") + "下载成功，使用原始歌词");
+                                break;
+                            case 2:
+                                File.WriteAllText(lrcFile, tlrc, _Encoding);
+                                Console.WriteLine(Path.ChangeExtension(fileName, ".lrc") + "下载成功，使用翻译歌词");
+                                break;
+                        }
+                    }
+                }
+                else
+                    Console.WriteLine(fileName + "无歌词文件");
+            }
+            Console.WriteLine("下载完成，按任意键退出");
+            Console.ReadKey();
         }
     }
 }
